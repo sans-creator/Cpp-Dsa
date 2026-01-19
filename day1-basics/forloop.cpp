8d1dfa7a64992905e6a14cfe67f7ea6dbb180215
sans-creator <sanskarj163@gmail.com>
2026-01-19 23:57:53 +0530

    day1 learning basics of cpp

--- day1-basics/forloop.cpp
+++ day1-basics/forloop.cpp
@@ -1,7 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    for(int i=0;i<=10;i++){
-        cout <<"striver"<<endl;
+    // for(int i=0;i<=10;i++){
+    //     cout <<"striver"<<endl;
+    // }
+    int i=1;
+    while(i<=5){
+        cout <<"striver"<< i <<endl;
+        i++;
+        
+
     }
+
 }
